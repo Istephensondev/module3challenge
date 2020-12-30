939a764fc4c4a93a382b0c96e9d5c83a90b678c0
Ivy Stephenson <istephensondev@gmail.com>
2020-12-30 12:29:28 -0600

    creating function for generating password

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,29 @@
 // Assignment code here
 
-function generatePassword(lengthone, charsetone) {
-  var length = lengthone,
-      charset = "abcde",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+function generatePassword(a, b, c, d, e) {
+  var length = a,
+        charset = b + c + d + e,
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+};
+
+// creating a function for generating the password
+
+var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
+  while (promptNumberOfChar < 8 || promptNumberOfChar > 128 ) {
+    window.alert('You must enter a number between 8 and 128!');
+    var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
   }
-  return retVal;
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  
-  var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
-  while (promptNumberOfChar < 8 || promptNumberOfChar > 128 ) {
-    window.alert('You must enter a number between 8 and 128!');
-    var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
-  }
-
-  var password = generatePassword(promptNumberOfChar);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
